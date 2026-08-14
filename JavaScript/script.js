0e6a17b716408eb7467cd5e7f0dc49f2c84b8e18
JoanCarolin <Joan-Carolin@web.de>
2026-08-14 11:43:01 +0200

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,25 @@
 const follower = document.querySelector(".cursor-follower");
 
+let mouseX = 0;
+let mouseY = 0;
+
+let followerX = 0;
+let followerY = 0;
+
 document.addEventListener("mousemove", function(event) {
-    follower.style.left = event.clientX + "px";
-    follower.style.top = event.clientY + "px";
+    mouseX = event.clientX;
+    mouseY = event.clientY;
 });
+
+function animate() {
+
+    followerX += (mouseX - followerX) * 0.1;
+    followerY += (mouseY - followerY) * 0.1;
+
+    follower.style.left = followerX + "px";
+    follower.style.top = followerY + "px";
+
+    requestAnimationFrame(animate);
+}
+
+animate();
