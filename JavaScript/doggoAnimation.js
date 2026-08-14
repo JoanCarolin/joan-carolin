630dc47cf40d3fc88ad8628abea7afc59aa95d42
JoanCarolin <Joan-Carolin@web.de>
2026-08-14 16:36:44 +0200

    Update doggoAnimation.js

--- JavaScript/doggoAnimation.js
+++ JavaScript/doggoAnimation.js
@@ -7,23 +7,15 @@ const doggo = document.querySelector(".frame-animation img");
 if (doggo) {
 
     const doggoFrames = [
-        "images/Doggo_Run_cycle-1.png",
-        "images/Doggo_Run_cycle-2.png",
-        "images/Doggo_Run_cycle-3.png",
-        "images/Doggo_Run_cycle-4.png",
-        "images/Doggo_Run_cycle-5.png",
-        "images/Doggo_Run_cycle-6.png",
-        "images/Doggo_Run_cycle-7.png",
-        "images/Doggo_Run_cycle-8.png"
+        "../images/Doggo_Run_cycle-1.png",
+        "../images/Doggo_Run_cycle-2.png",
+        "../images/Doggo_Run_cycle-3.png",
+        "../images/Doggo_Run_cycle-4.png",
+        "../images/Doggo_Run_cycle-5.png",
+        "../images/Doggo_Run_cycle-6.png",
+        "../images/Doggo_Run_cycle-7.png",
+        "../images/Doggo_Run_cycle-8.png"
     ];
-
-    const doggoImages = [];
-
-    doggoFrames.forEach((src) => {
-        const img = new Image();
-        img.src = src;
-        doggoImages.push(img);
-    });
 
     let currentDoggoFrame = 0;
 
@@ -31,11 +23,11 @@ if (doggo) {
 
         currentDoggoFrame++;
 
-        if (currentDoggoFrame >= doggoImages.length) {
+        if (currentDoggoFrame >= doggoFrames.length) {
             currentDoggoFrame = 0;
         }
 
-        doggo.src = doggoImages[currentDoggoFrame].src;
+        doggo.src = doggoFrames[currentDoggoFrame];
 
-    }, 70);
+    }, 100);
 }
