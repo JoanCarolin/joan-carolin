cb2a0173f39e1558a41e76daad3f4c25ec0cc66e
JoanCarolin <Joan-Carolin@web.de>
2026-08-14 16:38:53 +0200

    Update doggoAnimation.js

--- JavaScript/doggoAnimation.js
+++ JavaScript/doggoAnimation.js
@@ -7,14 +7,14 @@ const doggo = document.querySelector(".frame-animation img");
 if (doggo) {
 
     const doggoFrames = [
-        "../images/Doggo_Run_cycle-1.png",
-        "../images/Doggo_Run_cycle-2.png",
-        "../images/Doggo_Run_cycle-3.png",
-        "../images/Doggo_Run_cycle-4.png",
-        "../images/Doggo_Run_cycle-5.png",
-        "../images/Doggo_Run_cycle-6.png",
-        "../images/Doggo_Run_cycle-7.png",
-        "../images/Doggo_Run_cycle-8.png"
+        "images/Doggo_Run_cycle-1.png",
+        "images/Doggo_Run_cycle-2.png",
+        "images/Doggo_Run_cycle-3.png",
+        "images/Doggo_Run_cycle-4.png",
+        "images/Doggo_Run_cycle-5.png",
+        "images/Doggo_Run_cycle-6.png",
+        "images/Doggo_Run_cycle-7.png",
+        "images/Doggo_Run_cycle-8.png"
     ];
 
     let currentDoggoFrame = 0;
