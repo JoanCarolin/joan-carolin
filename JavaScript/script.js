9746c008d43df509052d8d307f395efc3b1075c2
JoanCarolin <Joan-Carolin@web.de>
2026-08-14 15:37:12 +0200

    Update script.js

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -99,3 +99,52 @@ setInterval(() => {
     animation.src = images[currentFrame].src;
 
 }, 70);
+
+
+
+
+//BirdieAnimation
+
+const frames = [
+    "frametoframe_bird_1.png",
+    "frametoframe_bird_2.png",
+    "frametoframe_bird_3.png",
+    "frametoframe_bird_4.png",
+    "frametoframe_bird_5.png",
+    "frametoframe_bird_6.png",
+    "frametoframe_bird_7.png",
+    "frametoframe_bird_8.png",
+    "frametoframe_bird_9.png",
+    "frametoframe_bird_10.png",
+    "frametoframe_bird_11.png",
+    "frametoframe_bird_12.png",
+    "frametoframe_bird_13.png",
+    "frametoframe_bird_14.png",
+    "frametoframe_bird_15.png",
+    "frametoframe_bird_16.png",
+    "frametoframe_bird_17.png",
+    "frametoframe_bird_18.png"
+];
+const animation = document.querySelector(".frame-animation img");
+
+const images = [];
+
+frames.forEach((src) => {
+    const img = new Image();
+    img.src = src;
+    images.push(img);
+});
+
+let currentFrame = 0;
+
+setInterval(() => {
+
+    currentFrame++;
+
+    if (currentFrame >= images.length) {
+        currentFrame = 0;
+    }
+
+    animation.src = images[currentFrame].src;
+
+}, 70);
